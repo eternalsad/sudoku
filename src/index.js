e11ef7320cc5e77f26d8166fbc2c4e3803b97494
eternalsad <sadikcartman@gmail.com>
2020-04-04 23:10:49 +0600

    fix: fixed backtracking algo

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
+var row = 0, col = 0;
 module.exports = function solveSudoku(matrix) {
   // your solution
   //return solve(matrix) ? matrix : false;
-  let ans = solve(matrix);
-  //printMatrix(matrix);
-  return matrix;
-}
 
-var row = 0, col = 0;
-function solve(matrix) {
-  //let row = 0, col = 0;
-  if (!findUnassignedLocation(matrix)) return true;
-  for (let num = 1; num <= 9; num++) {
-    if (isSafe(matrix, row, col, num)) {
-      matrix[row][col] = num;
-      if (solve(matrix)) {
-        return true;
-      }
-      matrix[row][col] = 0;
-    }
+  /*let matrix = [
+    [6, 5, 0, 7, 3, 0, 0, 8, 0],
+    [0, 0, 0, 4, 8, 0, 5, 3, 0],
+    [8, 4, 0, 9, 2, 5, 0, 0, 0],
+    [0, 9, 0, 8, 0, 0, 0, 0, 0],
+    [5, 3, 0, 2, 0, 9, 6, 0, 0],
+    [0, 0, 6, 0, 0, 0, 8, 0, 0],
+    [0, 0, 9, 0, 0, 0, 0, 0, 6],
+    [0, 0, 7, 0, 0, 0, 0, 5, 0],
+    [1, 6, 5, 3, 9, 0, 4, 7, 0]
+  ];*/
+
+  if (solve(matrix)) {
+    //printMatrix(matrix);
+    return matrix;
+  } else {
+    console.log("no solution");
   }
-  return false;
 }
 
-function findUnassignedLocation(matrix) {
+function unassigned_spot(matrix) {
+  let list = [];
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix.length; j++) {
-      if (matrix[i][j] == 0) {
-        row = i;
-        col = j;
-        return true;
-      }
+      if (matrix[i][j] == 0)
+        return [i, j]; // row, col
     }
   }
-  return false;
+  return [-1, -1]; // no 0's 
 }
+
+
 
 function isUsedInCol(matrix, col, num) {
   for (let i = 0; i < matrix.length; i++) {
@@ -68,8 +69,31 @@ function isSafe(matrix, row, col, num) {
     !isUsedInBox(matrix, (row - row % 3), (col - col % 3), num)) &&
     (matrix[row][col] == 0);
 }
+
+
+function solve(matrix) {
+  let cell = unassigned_spot(matrix);
+  if (cell[0] == "-1" || cell[1] == "-1") return true; // no zeros left
+  for (let num = 0; num <= 9; num++) {
+    if (isSafe(matrix, cell[0], cell[1], num)) {
+      matrix[cell[0]][cell[1]] = num; // try number
+      if (solve(matrix)) {
+        return true;
+      }
+      else {
+        matrix[cell[0]][cell[1]] = 0; // replace number with 0 try again
+      }
+    }
+  }
+  return false; // the current state isn't possible to solve
+}
+
 function printMatrix(matrix) {
   for (let i = 0; i < matrix.length; i++) {
-    console.log(matrix[i]);
+    let line = "";
+    for (let j = 0; j < matrix.length; j++) {
+      line += matrix[i][j] + " ";
+    }
+    console.log(line);
   }
 }
